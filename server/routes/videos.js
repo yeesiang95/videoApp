5b9d971bda5f497bef0accb32cb3d3ca4829788c
siang <siang@alinehrms.com>
2023-04-29 14:41:59 +0800

    First Commit

---
+++ server/routes/videos.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { verifyToken } from "../verifyToken.js";
+import { addVideo, addView, deleteVideo, getByTag, random, search, sub, trend, updateVideo } from "../controllers/videos.js";
+
+const router = express.Router();
+
+// create a video
+router.post("/",verifyToken,addVideo);
+router.put("/:id",verifyToken,updateVideo);
+router.delete("/:id",verifyToken,deleteVideo)
+router.get("/find/:id",addVideo)
+router.get("/view/:id",addView)
+router.get("/trend",trend)
+router.get("/random",random)
+router.get("/sub",verifyToken,sub)
+router.get("/tags",getByTag)
+router.get("/search",search)
+
+export default router;
